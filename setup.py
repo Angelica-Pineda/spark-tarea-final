92e38138b212841d765ec079d9a39e29a79867a1
Angelica Pineda <angelicap2298@gmail.com>
2026-04-12 21:17:31 +0200

    Configuraciones finales para generar el .whl del paquete motor_ingesta

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="motor-ingesta",
     version="0.1.0",
-    author="Nombre del alumno",
-    author_email="alumno@ucm.es",
+    author="Angelica Pineda",
+    author_email="jepineda@ucm.es",
     description="Motor de ingesta para el curso de Spark",
     long_description="Motor de ingesta para el curso de Spark",
     long_description_content_type="text/markdown",
-    url="https://github.com/nombrealumno",
+    url="https://github.com/Angelica-Pineda",
     python_requires=">=3.8",
     packages=find_packages(),
-    package_data={"motor_ingesta": ["resources/*.csv"]}
+    package_data={"motor_ingesta": ["resources/*.csv"]},
+    include_package_data=True,
+    install_requires=[
+        "loguru==0.7.1",
+        "pandas>=2.0.0",
+        "numpy<2.0.0" # Añadido para la compatibilidad en Databricks
+    ]
 )
+
